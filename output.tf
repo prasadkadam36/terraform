8e736cbdc65e58d59ca120617029774b7abf9f05
prasadkadam36 <33347427+prasadkadam36@users.noreply.github.com>
2025-03-17 17:56:45 +0530

    Delete output.tf

--- output.tf
+++
@@ -1,8 +0,0 @@
-output "aks_name" {
-  value = azurerm_kubernetes_cluster.aks.name
-}
-
-output "kube_config_raw" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
-  sensitive = true
-}
