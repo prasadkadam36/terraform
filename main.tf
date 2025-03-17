807a910780466bbde5746494ee4784ef2895b8d3
prasadkadam36 <33347427+prasadkadam36@users.noreply.github.com>
2025-03-17 18:33:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 provider "azurerm" {
   features {
-  subscription_id = var.subscription_id
-  client_id       = var.client_id       # optional if you're using ARM_CLIENT_ID env var
-  use_msi         = true
+
   }
 }
 
