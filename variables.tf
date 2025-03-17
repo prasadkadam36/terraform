ef96d4e7e239296f67e9793fdbc8f0fd6f27d3f6
prasadkadam36 <33347427+prasadkadam36@users.noreply.github.com>
2025-03-17 17:22:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,36 +1,5 @@
-provider "azurerm" {
-  features {}
-  use_msi = true
-}
-
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.aks.kube_config[0].host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config[0].cluster_ca_certificate)
-}
-
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_name
-  location            = var.region
-  resource_group_name = var.resource_group
-  dns_prefix          = "${var.aks_name}-dns"
-
-  default_node_pool {
-    name       = "default"
-    node_count = var.node_count
-    vm_size    = var.node_vm_size
-    vnet_subnet_id = var.subnet_id
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = var.tags
-}
-
-output "kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_config_raw
-  sensitive = true
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
 }
